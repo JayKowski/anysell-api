e845333d8ec17de1ac898583b6190736cd0a2250
JayKowski <jaymaina5@gmail.com>
2023-05-17 02:34:28 +0300

    renamed user password to accomodate hashing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_13_190222) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_15_203455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,7 +77,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_13_190222) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.text "location"
     t.string "user_type"
     t.string "mobile_number"
