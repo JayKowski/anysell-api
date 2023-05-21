46961c87d9a3fb272825cc65ac3042731c833bfb
JayKowski <jaymaina5@gmail.com>
2023-05-22 02:26:43 +0300

    added helper method to save pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,19 @@
 class Picture < ApplicationRecord
+    belongs_to :product
+
+    def self.create_pictures(pictures, product)
+        @pictures = []
+
+        # CLOUDINARY UPLOADS FIRST, then get links!
+        pictures.each do |p|
+            @picture = Picture.create!({ link_url: p[0], caption: p[1], product_id: product.id })
+            @picture && @pictures << @picture
+        end
+
+        if @pictures.length == pictures.length
+            return [true, @pictures]
+        else
+            return [false, @pictures]
+        end
+    end
 end
